3a4f9c3c39a0be46b7bb345c09bf2180e1b514af
Banji Alo <banji.alo@gmail.com>
2022-07-19 20:23:50 +1000

    Added my project

---
+++ blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Post
+
+# Register your models here.
+#add view to admin page
+admin.site.register(Post)
+
+
---
+++ blog/models.py
@@ -0,0 +1,20 @@
+#from email.policy import default
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+# Create your models here.
+#post model
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE) #delete post if a user is deleted
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
